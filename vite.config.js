ddf4bae7d6e30d4cf5b3683c3537b30c4f7c48bc
Lucas Hansell <lucashansell@gmail.com>
2024-11-22 12:39:49 -0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/YOUR_REPOSITORY_NAME",
+  base: "./YOUR_REPOSITORY_NAME",
   plugins: [react()],
 })
